7365f3c43015fc9b8c966fb6554111120ad4279a
lingling.yang <lingling.yang@@rograndec.com>
2017-09-05 18:16:16 +0800

    为了更新

--- src/components/console/user/PersonalInfo.vue
+++ src/components/console/user/PersonalInfo.vue
@@ -1,15 +1,9 @@
 <template>
   <div class="user-personal-info">
-    <br>
-    <br>
-    <br>
-    <br>
-    我是控制台内容部分。。。我是控制台内容部分。。。
-
-    <br>
-    <br>
-    <br>
-    <br>
+    <div class="edit-img">
+      <img src=""/>
+      <span>编辑头像</span>
+    </div>
   </div>
 </template>
 
@@ -27,4 +21,26 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.user-personal-info {
+  padding: 70px 4.2%;
+  background: #fff;
+}
+.edit-img {
+  width: 7.58%;
+  margin-right: 6.5%;
+  text-align:center;
+}
+.edit-img img {
+  width: 100%;
+  height: auto;
+  margin-bottom: 6px;
+}
+.edit-img img, .edit-img span{
+  display: block;
+}
+.edit-img span {
+  color: #546AAC;
+  letter-spacing: 0.69px;
+  line-height: 22px;
+}
 </style>
