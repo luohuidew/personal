09cd9119ec9468f96f4c62237d25dc7f3feac48f
lingling.yang <lingling.yang@@rograndec.com>
2017-09-06 16:25:45 +0800

    为了更新暂时提交的

--- src/components/console/user/PersonalInfo.vue
+++ src/components/console/user/PersonalInfo.vue
@@ -6,11 +6,43 @@
     </div>
     <div class="infos">
       <h2>基础信息</h2>
-      <ul>
-        <li class="nikeName">
-          <span>用户昵称</span><div contentEditable="false" class="name">沙枫</div><div class="edit-btn"><i><img src="../../../assets/icon-edit.png"/></i><em>编辑</em></div>
+      <ul class="line1">
+        <li class="nikeName" :v-model="usefulData">
+          <span>用户昵称</span><input :disabled="true" class="name" v-model="usefulData.nikeName"/><div class="edit-btn" @click="usefulData.changeNick = true"><i><img src="../../../assets/icon-edit.png"/></i><em>编辑</em></div>
+        </li>
+        <li class="common email">
+          <span>绑定邮箱</span><p>*****@shihuilaw.com</p><div class="edit-btn"><em>修改</em></div>
+        </li>
+        <li class="common tel">
+          <span>绑定手机</span><p>186****4563</p><div class="edit-btn"><em>解绑</em></div>
         </li>
       </ul>
+      <h2>安全设置</h2>
+      <ul class="line2">
+        <li class="common autonym">
+          <span>实名认证</span><p>未认证</p><div class="edit-btn"><em>实名认证</em></div>
+        </li>
+        <li class="common sign">
+          <span>签名管理</span><p><img src="../../../assets/sign-img.png"/></p><div class="edit-btn"><em>设置</em></div>
+        </li>
+        <li class="common account">
+          <span>账号安全</span><div class="edit-btn"><em>修改密码</em></div>
+        </li>
+      </ul>
+      <div class="attention">
+        <i>*</i><p>接受期权授予协议，必须要完成实名认证才能顺利签字。</br>实名认证审核时间约为1个工作日 ，请您合理安排认证时间，避免耽误签字。</p>
+      </div>
+      <el-dialog title="修改昵称" :visible.sync="usefulData.changeNick" size="tiny" :before-close="handleClose">
+      <el-form :model="usefulData">
+        <el-form-item label="昵称" :label-width="usefulData.formLabelWidth">
+          <el-input v-model="usefulData.nickName" auto-complete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="changeNick = false">取 消</el-button>
+        <el-button type="primary" @click="changeNick = false">确 定</el-button>
+      </span>
+    </el-dialog>
     </div>
   </div>
 </template>
@@ -20,17 +52,38 @@ export default {
   name: 'user-personal-info',
   data() {
     return {
+      usefulData: {
+        nikeName: '沙枫',
+        editStatus: false, // 默认输入框是不可编辑的
+        changeNick: false, // 默认昵称是不可编辑的
+        formLabelWidth: '120px',
+      },
     };
   },
   methods: {
+    toggle() {
+      this.usefulData.edtiStatus = !this.usefulData.edtiStatus;
+      console.log(this.usefulData.edtiStatus);
+    },
+    handleClose(done) {
+      this.$confirm('确认关闭？').then(() => {
+        done();
+      }).catch(() => {});
+    },
   },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+body {
+  -webkit-user-select:none;
+  -moz-user-select:none;
+  -ms-user-select:none;
+  user-select:none;
+}
 .user-personal-info {
-  padding: 70px 4.2%;
+  padding: 70px 4.2% 130px;
   background: #fff;
   overflow: hidden;
 }
@@ -57,7 +110,7 @@ export default {
   line-height: 22px;
 }
 .infos {
-  width: 32.7%;
+  width: 40%;
 }
 .infos h2 {
   margin-bottom: 22px;
@@ -66,7 +119,11 @@ export default {
   letter-spacing: 1px;
   line-height: 24px;
 }
+.infos ul {
+  border-bottom: 1px solid #EBEBEB;
+}
 .infos ul li{
+  margin-bottom: 26px;
   line-height: 22px;
   overflow: hidden;
 }
@@ -78,31 +135,67 @@ export default {
   float: left;
   letter-spacing: 0.69px;
 }
+.name {
+  border: 0 none;
+}
 .infos ul li span {
   margin-right: 6%;
   color: #999;
 }
-.infos ul li .name {
+.infos ul li .name, .infos ul li p {
+  display: inline-block;
   width: 60%;
   color: #666;
 }
 .infos ul li .edit-btn {
   float: right;
+  min-width: 55px;
 }
-.infos ul li .edit-btn em {
+.infos ul li em {
   color: #546AAC;
   letter-spacing: 1px;
+  font-style: normal;
 }
 .infos ul li .edit-btn em, .infos ul li .edit-btn i {
-  float: right;
+  float: left;
+  cursor: default;
 }
 .infos ul li .edit-btn i{
+  position: relative;
+  top: 1px;
   width: 12px;
   height: 12px;
-  margin-right: 2%;
+  margin-right: 18%;
 }
 .infos ul li .edit-btn i img{
   width: 100%;
   height: 100%;
+}
+.infos ul li.common .edit-btn em {
+  float: right;
+}
+.line1 {
+  margin-bottom: 30px;
+}
+.line2 {
+  margin-bottom: 18px;
+}
+.attention {
+  overflow: hidden;
+  line-height: 22px;
+}
+.attention i{
+  float: left;
+  display: block;
+  width: 3%;
+  height: 66px;
+  color: #C73131;
+}
+.attention p {
+  float: left;
+  display: block;
+  width: 97%;
+  color: #3F3F3F;
+  letter-spacing: 0.69px;
 }
 </style>
