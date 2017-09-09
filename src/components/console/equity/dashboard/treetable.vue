3aae9eea9e9c55c55a9dda946eadcd2872527ad9
wuzhj <wuzhj@shihuilaw.com>
2017-09-09 13:45:33 +0800

    treetable组件三期

--- src/components/console/equity/dashboard/treetable.vue
+++ src/components/console/equity/dashboard/treetable.vue
@@ -3,10 +3,15 @@
     <el-table :data="treeData">
       <el-table-column type="expand">
         <template scope="props">
-          <el-table :data="props.children" inline :show-header="false" :border="false">
-            <el-table-column prop="name"></el-table-column>
-            <el-table-column prop="money"></el-table-column>
-            <el-table-column prop="name"></el-table-column>
+          <el-table ref="expandTable" :data="props.row.children" inline :show-header="false" :border="false">
+            <el-table-column type="index" width="55"></el-table-column>
+            <el-table-column prop="shareholderName"></el-table-column>
+            <el-table-column prop="rounds"></el-table-column>
+            <el-table-column prop="registeredCapital"></el-table-column>
+            <el-table-column prop="rate"></el-table-column>
+            <el-table-column>
+              <template scope="scope"></template>
+            </el-table-column>
           </el-table>
         </template>
       </el-table-column>
@@ -17,6 +22,7 @@
           {{scope.row[column.dataIndex]}}
         </template>
       </el-table-column>
+      <el-table-column label="操作"></el-table-column>
     </el-table>
   </div>
 </template>
@@ -25,56 +31,77 @@ export default {
   name: 'tree-table',
   data() {
     return {
-      treeData: [{
-        shareholderAbbreviation: '真格基金',
-        rounds: '天使轮',
-        registeredCapital: '80000',
-        rate: '0.4%',
-        children: [
-          {
-            shareholderName: '某某某科技有限公司',
-            rounds: '天使轮',
-            registeredCapital: '20000',
-            rate: '0.2%',
-          }, {
-            shareholderName: '锤子科技有限公司',
-            rounds: '天使轮',
-            registeredCapital: '40000',
-            rate: '0.4%',
-          },
-        ],
-      }, {
-        shareholderAbbreviation: '真格基金',
-        rounds: '天使轮',
-        registeredCapital: '80000',
-        rate: '0.4%',
-        children: [
-          {
-            shareholderName: '某某某科技有限公司',
-            rounds: '天使轮',
-            registeredCapital: '20000',
-            rate: '0.2%',
-          }, {
-            shareholderName: '某某某科技有限公司',
-            rounds: '天使轮',
-            registeredCapital: '40000',
-            rate: '0.4%',
-          },
-        ],
-      }],
+      totalMoney: '140000',
+      treeData: [],
     };
   },
   props: {
     treeClomns: {},
   },
   created() {
+    const testdata = [{
+      shareholderAbbreviation: '真格基金',
+      rounds: '天使轮',
+      registeredCapital: '80000',
+      children: [
+        {
+          shareholderName: '某某某科技有限公司',
+          rounds: '天使轮',
+          registeredCapital: '34000',
+        }, {
+          shareholderName: '锤子科技有限公司',
+          rounds: '天使轮',
+          registeredCapital: '46000',
+        },
+      ],
+    }, {
+      shareholderAbbreviation: '真格基金',
+      rounds: '天使轮',
+      registeredCapital: '60000',
+      children: [
+        {
+          shareholderName: '某某某科技有限公司',
+          rounds: '天使轮',
+          registeredCapital: '20000',
+        }, {
+          shareholderName: '某某某科技有限公司',
+          rounds: '天使轮',
+          registeredCapital: '40000',
+        },
+      ],
+    }];
+    this.treeData = testdata;
+    testdata.forEach((value, index) => {
+      if (value.children.length !== 0) {
+        value.children.forEach((key, i) => {
+          const rate = this.getPercent(key.registeredCapital, this.totalMoney);
+          this.treeData[index].children[i].rate = rate;
+        });
+      }
+      const r = this.getPercent(value.registeredCapital, this.totalMoney);
+      this.treeData[index].rate = r;
+    });
   },
   methods: {
+    getPercent(num, total) {
+      const number = parseFloat(num);
+      const totals = parseFloat(total);
+      if (isNaN(number) || isNaN(totals)) {
+        return '-';
+      }
+      if (totals < 0) {
+        return '0%';
+      }
+      const rate = Math.round((number / totals) * 10000) / 100.00;
+      return `${rate}%`;
+      // return totals <= 0 ? '0%' : ((Math.round((number / totals) * 10000) / 100.00) + '%');
+    },
   },
 };
 </script>
 <style scoped>
 .el-table__expanded-cell{padding: 0px;}
 .el-table td > div{border: none;}
+.el-table__expanded-cell .el-table th.is-leaf, .el-table td{border:none;}
 </style>
 
