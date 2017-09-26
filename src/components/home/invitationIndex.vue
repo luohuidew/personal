e3ec469fcb04745668ee265a744f5b60b8f4cc85
liull <liull@shihuilaw.com>
2017-09-26 17:19:09 +0800

    参与方邮件邀请

--- src/components/home/invitationIndex.vue
+++ src/components/home/invitationIndex.vue
@@ -3,15 +3,15 @@
     <div class="top"><img src="../../assets/login-logo.png"/></div>
     <div class="contain">
       <div class="main-con">
-        <p>XXXXXX公司 正在邀请您启用</p>
-        <p class="fontColor">CapTable - XXXXXXXXXX期权管理系统</p>
+        <div class="inputDiv"><input class="f-control" v-model="companyName">正在邀请您启用</div>
+        <p class="fontColor">CapTable - 精益股权期权管理系统</p>
         <div class="noneBlock"></div>
         <span>如果您还没有股书账号，请注册新的账号接受邀请</span>
         <span>您也可以用已有的账号接受邀请</span>
         <div class="noneBlock"></div>
         <div class="btnDiv">
-          <router-link class="return btn" :to="{ path: '/invitation_register' }">注册新账号并接受邀请</router-link>
-          <router-link class="return btn1" :to="{ path: '/invitation_login' }">登录已有账号接受邀请</router-link>
+          <el-button class="return btn" @click="registerLink">注册新账号并接受邀请</el-button>
+          <el-button class="return btn1" @click="loginLink">登录已有账号接受邀请</el-button>
         </div>
       </div>
     </div>
@@ -19,11 +19,13 @@
 </template>
 
 <script>
+import pService from '../../service/participator';
+
 export default {
   name: 'InvitationIndex',
   data() {
     return {
-      companyName: undefined,
+      companyName: undefined, // 公司名
     };
   },
   created() {
@@ -32,12 +34,46 @@ export default {
   methods: {
     initData() {
       // TODO：通过code获取公司名称
+      this.companyCode = location.hash.split('/').pop();
+      pService.getCompanyInfoByCode(this.companyCode).then((resp) => {
+        if (resp.code.code === 200) {
+          this.companyName = resp.data.companyName;
+        }
+      }, (resp) => {
+        this.$message.error(resp.code.msg);
+      });
+    },
+    registerLink() {
+      this.$router.push({
+        name: 'InvitationRegister',
+        params: { code: this.companyCode },
+      });
+    },
+    loginLink() {
+      this.$router.push({
+        name: 'InvitationLogin',
+        params: { code: this.companyCode },
+      });
     },
   },
 };
 </script>
 
 <style scoped>
+.inputDiv{
+  color:#555;
+  font-size: 24px;
+  text-align: center;
+  padding-bottom: 15px;
+}
+.f-control{
+  border: none;
+  font-size: 24px;
+  text-align: right;
+  padding-right: 10px;
+  font-family: 'HanHei SC', 'PingFang SC', '\5FAE\8F6F\96C5\9ED1', sans-serif;
+  color: #555;
+}
 .top{
   height: 60px;
   background: #fff;
